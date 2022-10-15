92780420347e3d73564ed825af5ed4d4fab63399
daniel2csv <115846259+daniel2csv@users.noreply.github.com>
2022-10-15 11:35:39 -0500

    Initial commit

---
+++ Assets/scripts/playerMove.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 5;
+
+    public float turnSmoothTime = 0.1f;
+
+    float turnSmoothVelocity;
+
+    public Transform cam; 
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+
+        Vector3 direction = new Vector3(horizontal,0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f){
+            //rotaciones
+            float targetAngle=Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            //movimiento
+            controller.Move(moveDir.normalized*speed*Time.deltaTime);
+        }
+
+    }
+}
